b9798a4516b217059df1d8f508ab9f1822324908
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-16 00:46:05 -0500

    Creating class for point on map and creating first instance of map inside game

--- Map.cs
+++ Map.cs
@@ -2,6 +2,13 @@ namespace TreehouseDefense
 {
     class Map
     {
+        public  int Width;
+        public  int Height;
         
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
