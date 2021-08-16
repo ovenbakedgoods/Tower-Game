c3cacf7ea6d1b60e89ad0347e329e29c0174dca9
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-16 00:11:23 -0500

    First commit

--- Game.cs
+++ Game.cs
@@ -5,6 +5,13 @@ namespace TreehouseDefense
         public static void Main()
         {
             Tower tower = new Tower();
+
+            Map map = new Map();
+            //assign width and height to new map
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
         }
     }
 }
--- Map.cs
+++ Map.cs
@@ -2,6 +2,14 @@ namespace TreehouseDefense
 {
     class Map
     {
-        
+       public readonly int Width;
+       public readonly int Height;
+       
+       Map(int width, int height)
+       {
+           Width = width;
+           Height = height;
+       }
+
     }
 }
